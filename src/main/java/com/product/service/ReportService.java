c4fe6271f238c8fa2682773ad893060e1ad09381
Rahul Sahu <therahulsahu7@gmail.com>
2022-06-28 23:12:07 +0530

    Added Report generation logic

---
+++ src/main/java/com/product/service/ReportService.java
@@ -0,0 +1,44 @@
+package com.product.service;
+
+import com.product.model.Product;
+import com.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+
+@Service
+public class ReportService {
+
+    @Autowired
+    private ProductRepository repository;
+
+
+    public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+        String path = "";
+        List<Product> employees = repository.findAll();
+        //load file and compile it
+        File file = ResourceUtils.getFile("classpath:products.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(employees);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy", "Jasper Reports");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, "products.html");
+        }
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint, "products.pdf");
+        }
+        return "report generated in path : " + path;
+    }
+}
